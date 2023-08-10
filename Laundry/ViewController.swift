42f872e066fcf15f72eef7b161d82ed9c83f77c0
Ranjit-Mahto <iosdev.ranjit.mahto@gmail.com>
2023-08-10 08:10:11 +0530

    Initial Commit

---
+++ Laundry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Laundry
+//
+//  Created by Ranjit Mahto on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
